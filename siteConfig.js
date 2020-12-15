4369447d3aa1f2422d0ff6911d2674d390172631
Ben Matthews <ben@montfort.io>
2020-12-15 07:29:49 +0000

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -3,20 +3,20 @@ module.exports = {
     siteUrl: `https://demo.jamify.org`,
 
     // Enable infinite scroll (default: true)
-    infiniteScroll: true,
+    infiniteScroll: false,
 
     // Initial number fetched, scrolling lazy loads posts one by one
     // If infinite scroll is disabled: maximum number of post shown per page
-    postsPerPage: 3,
+    postsPerPage: 60,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `Flourish`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: `Flourish in life and love`,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `Flourish`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
